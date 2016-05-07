0a0983895ad77627943f10217f9fe3cafbe8925d
sunbinbin <sunbinbin@imdada.cn>
2016-05-07 14:36:20 +0800

    [ADD] 简单的例子

--- examples/course1/index.js
+++ examples/course1/index.js
@@ -3,14 +3,45 @@ import React from 'react'
 import ReactDOM,{ render } from 'react-dom'
 
 var Hello = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'world'
+        };
+    },
+    propTypes: {
+        name: React.PropTypes.string
+    },
     render: function(){
-        return <div>Hello {this.props.name}</div>;
+        return <div className="test">Hello {this.props.name}</div>;
         // return React.createElement('div', {}, 'Hello ',this.props.name);
     }
 });
 
+var LikeButton = React.createClass({
+    getInitialState: function() {
+        return {
+            liked: false
+        };
+    },
+    handleClick: function(event) {
+        this.setState({liked: !this.state.liked});
+    },
+    render: function() {
+        var text = this.state.liked ? 'like' : 'haven\'t liked';
+        return (
+            <p onClick={this.handleClick}>
+                You {text} this. Click to toggle.
+            </p>
+        );
+    }
+});
+
 ReactDOM.render(
-    <Hello name="world" />,
+    <div>
+        <Hello name={"world"} />
+        <LikeButton />
+    </div>,
+    
     // React.createElement(Hello, {name: "world"}),
     document.getElementById('root')
 )
